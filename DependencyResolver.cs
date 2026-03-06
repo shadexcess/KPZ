8699a700740fd7e01eb534dc9f7c2f3d709c47b6
shadexcess <sarewuyyr@gmail.com>
2026-03-06 16:25:45 +0200

    Basic topological sort added

--- DependencyResolver.cs
+++ DependencyResolver.cs
@@ -11,10 +11,40 @@ public class DependencyResolver
 
 	public List<Package> Resolve()
 	{
-		List<Package> packages = new List<Package>();
+        HashSet<Package> visited = new HashSet<Package>();
+        Stack<Package> stack = new Stack<Package>();
+        IEnumerable<Package> nodes = graph.GetAllPackages();
 
-		// topological sort
+        foreach (Package node in nodes)
+        {
+            if (!visited.Contains(node))
+            {
+                DFS(node, visited, stack);
+			}
+		}
 
-		return packages;
+        List<Package> packagesSorted = new List<Package>();
+		while (stack.Count != 0)
+		{
+			packagesSorted.Add(stack.Pop());
+		}
+
+        return packagesSorted;
+	}
+
+	private void DFS(Package node, HashSet<Package> visited, Stack<Package> stack)
+	{
+		visited.Add(node);
+		List<Package> neighbors = graph.GetDependencies(node);
+
+        foreach (Package neighbor in neighbors)
+		{
+			if (!visited.Contains(neighbor))
+			{
+                DFS(neighbor, visited, stack);
+			}
+		}
+
+		stack.Push(node);
 	}
 }
--- Program.cs
+++ Program.cs
@@ -7,21 +7,12 @@
         FileReader reader = new FileReader();
         DependencyGraph graph = reader.ReadFile(filePath);
 
-        //DependencyResolver resolver = new DependencyResolver(graph);
-        //List<Package> order = resolver.Resolve();
+        DependencyResolver resolver = new DependencyResolver(graph);
+        List<Package> order = resolver.Resolve();
 
-        //foreach (Package package in order)
-        //{
-        //    Console.WriteLine(package.Name + "-" + package.Version);
-        //}
-
-
-
-        // to test if it works
-        IEnumerable<Package> list = graph.GetAllPackages();
-        foreach (var element in list)
+        foreach (Package package in order)
         {
-            Console.WriteLine(element);
+            Console.WriteLine(package.Name + "-" + package.Version);
         }
     }
 }
