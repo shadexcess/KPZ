22739f07af6b00117799338badfe26cd29d69fd6
shadexcess <sarewuyyr@gmail.com>
2026-04-03 22:23:37 +0300

    Added other classes for exception handling

--- PackageFormatException.cs
+++ PackageFormatException.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="CyclicDependencyException.cs" company="PlaceholderCompany">
+﻿// <copyright file="PackageFormatException.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
@@ -7,33 +7,33 @@ namespace lab1;
 using System;
 
 /// <summary>
-/// Represents an error that occurs when a dependency cycle is detected in the package dependency graph.
+/// Represents an error that occurs when a package line does not conform to the expected format.
 /// </summary>
-public class CyclicDependencyException : DependencyException
+public class PackageFormatException : DependencyException
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="CyclicDependencyException"/> class.
+    /// Initializes a new instance of the <see cref="PackageFormatException"/> class.
     /// </summary>
-    public CyclicDependencyException()
+    public PackageFormatException()
     {
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="CyclicDependencyException"/> class with a specified error message.
+    /// Initializes a new instance of the <see cref="PackageFormatException"/> class with a specified error message.
     /// </summary>
     /// <param name="message">The message that describes the error.</param>
-    public CyclicDependencyException(string message)
+    public PackageFormatException(string message)
         : base(message)
     {
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="CyclicDependencyException"/> class
+    /// Initializes a new instance of the <see cref="PackageFormatException"/> class
     /// with a specified error message and a reference to the inner exception that is the cause of this exception.
     /// </summary>
     /// <param name="message">The error message that explains the reason for the exception.</param>
     /// <param name="innerException">The exception that caused the current exception.</param>
-    public CyclicDependencyException(string message,  Exception innerException)
+    public PackageFormatException(string message, Exception innerException)
         : base(message, innerException)
     {
     }
