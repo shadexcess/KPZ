35998cde292b63e7fdb9c5a90f3235c0a12b2d58
shadexcess <sarewuyyr@gmail.com>
2026-04-24 14:09:16 +0300

    separated project files into folders

---
+++ lab1/Graph/DependencyGraph.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="DependencyGraph.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace lab1;
+
+/// <summary>
+/// Represents a dependency graph of packages and stores them and their dependency relationships.
+/// </summary>
+public class DependencyGraph
+{
+    private Dictionary<Package, List<Package>> dependencies;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DependencyGraph"/> class.
+    /// </summary>
+    public DependencyGraph()
+    {
+        this.dependencies = new Dictionary<Package, List<Package>>();
+    }
+
+    /// <summary>
+    /// Adds a package to the dependency graph. If the package already exists, it will not be added again.
+    /// </summary>
+    /// <param name="package">The package to add to the graph.</param>
+    public void AddPackage(Package package)
+    {
+        if (!this.dependencies.ContainsKey(package))
+        {
+            this.dependencies.Add(package, new List<Package>());
+        }
+    }
+
+    /// <summary>
+    /// Adds a dependency from one package to another.
+    /// </summary>
+    /// <param name="from">The package that depends on another package.</param>
+    /// <param name="to">The package being depended upon.</param>
+    public void AddDependency(Package from, Package to)
+    {
+        if (from.Equals(to))
+        {
+            return;
+        }
+
+        if (!this.dependencies[from].Contains(to))
+        {
+            this.dependencies[from].Add(to);
+        }
+    }
+
+    /// <summary>
+    /// Retrieves the list of packages that the specified package depends on.
+    /// </summary>
+    /// <param name="package">The package whose dependencies are to be retrieved.</param>
+    /// <returns>A list of packages that the specified package depends on. Returns an empty list if there are no dependencies.</returns>
+    public List<Package> GetDependencies(Package package)
+    {
+        if (this.dependencies.ContainsKey(package))
+        {
+            return this.dependencies[package];
+        }
+        else
+        {
+            return new List<Package>();
+        }
+    }
+
+    /// <summary>
+    /// Retrieves all packages in the dependency graph.
+    /// </summary>
+    /// <returns>An enumerable collection of all packages stored in the graph.</returns>
+    public IEnumerable<Package> GetAllPackages()
+    {
+        return this.dependencies.Keys;
+    }
+}
---
+++ lab1/Graph/DependencyResolver.cs
@@ -0,0 +1,87 @@
+﻿// <copyright file="DependencyResolver.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace lab1;
+
+using System;
+
+/// <summary>
+/// Resolves the installation order of packages based on their dependencies.
+/// Ensures that packages are installed in the correct order and detects dependency cycles.
+/// </summary>
+public class DependencyResolver
+{
+    private readonly DependencyGraph graph;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DependencyResolver"/> class with a dependency graph.
+    /// </summary>
+    /// <param name="graph">The <see cref="DependencyGraph"/> containing packages and their dependencies.</param>
+    public DependencyResolver(DependencyGraph graph)
+    {
+        this.graph = graph;
+    }
+
+    /// <summary>
+    /// Resolves the packages in an order that satisfies all dependencies.
+    /// </summary>
+    /// <returns>A list of <see cref="Package"/> objects sorted in dependency order.</returns>
+    public List<Package> Resolve()
+    {
+        HashSet<Package> visited = new HashSet<Package>();
+        Stack<Package> stack = new Stack<Package>();
+        IEnumerable<Package> packages = this.graph.GetAllPackages();
+        HashSet<Package> currentPath = new HashSet<Package>();
+
+        foreach (Package package in packages)
+        {
+            if (!visited.Contains(package))
+            {
+                this.DepthFirstSearch(package, visited, stack, currentPath);
+            }
+        }
+
+        List<Package> packagesSorted = new List<Package>();
+        while (stack.Count != 0)
+        {
+            packagesSorted.Add(stack.Pop());
+        }
+
+        packagesSorted.Reverse();
+
+        return packagesSorted;
+    }
+
+    /// <summary>
+    /// Performs a depth-first search to visit packages and detect cycles.
+    /// </summary>
+    /// <param name="currentPackage">The current <see cref="Package"/> being visited.</param>
+    /// <param name="visited">A set of packages that have already been visited.</param>
+    /// <param name="stack">A stack used to determine the installation order.</param>
+    /// <param name="currentPath">A set representing the current path to detect dependency cycles.</param>
+    /// <exception cref="Exception">Thrown if a dependency cycle is detected during traversal.</exception>
+    private void DepthFirstSearch(Package currentPackage, HashSet<Package> visited, Stack<Package> stack, HashSet<Package> currentPath)
+    {
+        visited.Add(currentPackage);
+        currentPath.Add(currentPackage);
+
+        List<Package> dependencyPackages = this.graph.GetDependencies(currentPackage);
+
+        foreach (Package dependency in dependencyPackages)
+        {
+            if (currentPath.Contains(dependency))
+            {
+                throw new CyclicDependencyException("Dependency cycle detected");
+            }
+
+            if (!visited.Contains(dependency))
+            {
+                this.DepthFirstSearch(dependency, visited, stack, currentPath);
+            }
+        }
+
+        currentPath.Remove(currentPackage);
+        stack.Push(currentPackage);
+    }
+}
