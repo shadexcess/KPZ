e60386f4caa04c0bd83c0b9f150c6b82d18f7c0a
shadexcess <sarewuyyr@gmail.com>
2026-04-25 21:53:56 +0300

    implemented HighestVersionStrategy

--- lab1/Strategies/FirstEncounteredStrategy.cs
+++ lab1/Strategies/FirstEncounteredStrategy.cs
@@ -4,8 +4,21 @@
 
 namespace lab1;
 
+/// <summary>
+/// Implements a dependency resolution strategy that selects the first encountered version
+/// of each package in the dependency graph without performing version comparison.
+/// </summary>
 public class FirstEncounteredStrategy : IResolutionStrategy
 {
+    /// <summary>
+    /// Resolves dependency conflicts by selecting the first encountered
+    /// package version for each package in the dependency graph.
+    /// </summary>
+    /// <param name="graph">The dependency graph containing packages and their relationships.</param>
+    /// <returns>
+    /// A dictionary where the key is the package name and the value is the first encountered version
+    /// of that package in the dependency traversal order.
+    /// </returns>
     public Dictionary<string, Package> ResolveConflicts(DependencyGraph graph)
     {
         Dictionary<string, HashSet<Package>> packagesVersions = ((IResolutionStrategy)this).GetVersionsForEachPackage(graph);
---
+++ lab1/Strategies/HighestVersionStrategy.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="HighestVersionStrategy.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace lab1;
+
+/// <summary>
+/// Implements a dependency resolution strategy that selects the highest
+/// available version of each package from the dependency graph.
+/// </summary>
+public class HighestVersionStrategy : IResolutionStrategy
+{
+    /// <summary>
+    /// Resolves dependency conflicts by choosing the highest version of each package.
+    /// </summary>
+    /// <param name="graph"> The dependency graph containing packages and their relationships.</param>
+    /// <returns>
+    /// A dictionary where the key is the package name and the value is the highest available version of that package.
+    /// </returns>
+    public Dictionary<string, Package> ResolveConflicts(DependencyGraph graph)
+    {
+        Dictionary<string, HashSet<Package>> packagesVersions = ((IResolutionStrategy)this).GetVersionsForEachPackage(graph);
+
+        var maxPackages = packagesVersions.ToDictionary(
+            kvp => kvp.Key,
+            kvp => kvp.Value
+                .OrderByDescending(p => new Version(p.version.TrimStart('v')))
+                .First());
+
+        return maxPackages;
+    }
+}
