3e84b147a567e3b6ecb4e02b33a7e64c63af75e7
shadexcess <sarewuyyr@gmail.com>
2026-04-25 14:53:34 +0300

    added implementation of FirstEncounteredStrategy

---
+++ lab1/Strategies/FirstEncounteredStrategy.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="FirstEncounteredStrategy.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace lab1;
+
+public class FirstEncounteredStrategy : IResolutionStrategy
+{
+    public Dictionary<string, Package> ResolveConflicts(DependencyGraph graph)
+    {
+        Dictionary<string, HashSet<Package>> packagesVersions = ((IResolutionStrategy)this).GetVersionsForEachPackage(graph);
+
+        Dictionary<string, Package> firstPackages = packagesVersions
+            .ToDictionary(x => x.Key, x => x.Value.First());
+
+        return firstPackages;
+    }
+}
