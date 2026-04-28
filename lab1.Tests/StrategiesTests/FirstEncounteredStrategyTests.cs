e454e85d62affc61ca139c27c25ccc28ce56a992
shadexcess <sarewuyyr@gmail.com>
2026-04-28 09:39:34 +0300

    added tests to verify whether a null value is returned when there are no conflicts

--- lab1.Tests/StrategiesTests/FirstEncounteredStrategyTests.cs
+++ lab1.Tests/StrategiesTests/FirstEncounteredStrategyTests.cs
@@ -5,6 +5,16 @@ using lab1;
 public class FirstEncounteredVersionStrategyTests
 {
     [Fact]
+    public void ResolveConflicts_ReturnsNull_WhenNoVersionConflicts()
+    {
+        DependencyGraph graph = TestGraphBuilder.BuildGraphNoConflictingVersions();
+        IResolutionStrategy strategy = new FirstEncounteredStrategy();
+        var result = strategy.ResolveConflicts(graph);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
     public void ResolveConflicts_ReturnsFirstEncounteredPackageVersion_WhenVersionConflict()
     {
         DependencyGraph graph = TestGraphBuilder.BuildGraphConflictingVersions();
--- lab1.Tests/StrategiesTests/HighestVersionStrategyTests.cs
+++ lab1.Tests/StrategiesTests/HighestVersionStrategyTests.cs
@@ -5,6 +5,16 @@ using lab1;
 public class HighestVersionStrategyTests
 {
     [Fact]
+    public void ResolveConflicts_ReturnsNull_WhenNoVersionConflicts()
+    {
+        DependencyGraph graph = TestGraphBuilder.BuildGraphNoConflictingVersions();
+        IResolutionStrategy strategy = new HighestVersionStrategy();
+        var result = strategy.ResolveConflicts(graph);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
     public void ResolveConflicts_ReturnsHighestPackageVersion_WhenVersionConflict()
     {
         DependencyGraph graph = TestGraphBuilder.BuildGraphConflictingVersions();
